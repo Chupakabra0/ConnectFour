5fc8382fb9bbfae1a4f4e6183ff3898d7dec523d
Chupakabra0 <rootalfa4@gmail.com>
2021-04-10 17:12:08 +0300

    More getters and << overloading

--- include/Board/Board.hpp
+++ include/Board/Board.hpp
@@ -2,13 +2,14 @@
 
 #include <array>
 #include <algorithm>
+#include <iostream>
 
 enum class MoveCharacters : char {
     NONE =
     #if defined(DEBUG)
-        '&',
+    '&',
     #else
-        ' ',
+    ' ',
     #endif
     FIRST_PLAYER = 'X',
     SECOND_PLAYER = 'O'
@@ -17,21 +18,75 @@ enum class MoveCharacters : char {
 template<short RowsCount, short ColumnsCount>
 class Board {
 public:
+    //------------------------------------------------- CTOR SECTION -------------------------------------------------//
+
     Board() {
         std::fill(this->field_.begin(), this->field_.end(), MoveCharacters::NONE);
     }
 
-    Board(const Board&)     = delete;
+    Board(const Board&) = delete;
     Board(Board&&) noexcept = default;
 
-    Board& operator=(const Board&)     = delete;
+    //--------------------------------------------- OPERATOR SECTION -------------------------------------------------//
+
+    Board& operator=(const Board&) = delete;
     Board& operator=(Board&&) noexcept = delete;
+
+    template<class Type>
+    friend auto& operator<<(std::basic_ostream<Type>& out, const Board& board) {
+        if (board.field_.empty()) {
+            return out;
+        }
+
+        for (auto i = 0; i < RowsCount; ++i) {
+            for (auto j = 0; j < ColumnsCount; ++j) {
+                out << static_cast<char>(board.GetCell(i, j));
+                if (j != ColumnsCount - 1) {
+                    out << " ";
+                }
+            }
+            if (i != RowsCount - 1) {
+                out << std::endl;
+            }
+        }
+
+        return out;
+    }
+
+    //----------------------------------------------- DTOR SECTION ---------------------------------------------------//
 
     ~Board() noexcept = default;
 
-     const auto& GetField() {
-         return this->field_;
-     }
+    //-------------------------------------------- ACCESSOR SECTION --------------------------------------------------//
+
+    [[nodiscard]] const auto& GetField() const {
+        return this->field_;
+    }
+
+    [[nodiscard]] constexpr auto& GetSize() const {
+        return this->boardSize_;
+    }
+
+    [[nodiscard]] constexpr auto& GetRows() const {
+        return RowsCount;
+    }
+
+    [[nodiscard]] constexpr auto& GetColumns() const {
+        return ColumnsCount;
+    }
+
+    //--------------------------------------------- METHOD SECTION ---------------------------------------------------//
+
+    [[nodiscard]] auto GetCell(const size_t row, const size_t column) const {
+        if (row >= RowsCount) {
+            throw std::out_of_range("Row index out of boundaries.");
+        }
+        else if (column >= ColumnsCount) {
+            throw std::out_of_range("Column index out of boundaries.");
+        }
+
+        return this->field_[row * RowsCount + column];
+    }
 
 private:
     static constexpr auto boardSize_ = RowsCount * ColumnsCount;
--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,7 @@
 int main(const int argc, const char* argv[]) {
     const auto board = std::make_unique<Board<6, 7>>();
 
+    std::clog << *board;
+
     return EXIT_SUCCESS;
 }
