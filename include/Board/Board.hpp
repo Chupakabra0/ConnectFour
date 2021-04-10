4ab82d58155bdc5a4e83d366ad021b669bf49116
Chupakabra0 <rootalfa4@gmail.com>
2021-04-10 19:39:09 +0300

    Added primitive moves

--- include/Board/Board.hpp
+++ include/Board/Board.hpp
@@ -4,15 +4,24 @@
 #include <algorithm>
 #include <iostream>
 
+enum class Row : short {
+    FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, COUNT
+};
+
+enum class Column : short {
+    FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, COUNT
+};
+
 enum class MoveCharacters : char {
     NONE =
     #if defined(DEBUG)
-    '&',
+        '&',
     #else
-    ' ',
+        ' ',
     #endif
     FIRST_PLAYER = 'X',
-    SECOND_PLAYER = 'O'
+    SECOND_PLAYER = 'O',
+    COUNT
 };
 
 template<short RowsCount, short ColumnsCount>
@@ -21,7 +30,9 @@ public:
     //------------------------------------------------- CTOR SECTION -------------------------------------------------//
 
     Board() {
-        std::fill(this->field_.begin(), this->field_.end(), MoveCharacters::NONE);
+        //std::fill(this->field_.begin(), this->field_.end(), MoveCharacters::NONE);
+        auto sym = 'A';
+        std::for_each(this->field_.begin(), this->field_.end(), [&](auto& a) { a = (char )MoveCharacters::NONE; });
     }
 
     Board(const Board&) = delete;
@@ -40,7 +51,7 @@ public:
 
         for (auto i = 0; i < RowsCount; ++i) {
             for (auto j = 0; j < ColumnsCount; ++j) {
-                out << static_cast<char>(board.GetCell(i, j));
+                out << static_cast<char>(board.GetCell(Row(i), Column(j)));
                 if (j != ColumnsCount - 1) {
                     out << " ";
                 }
@@ -63,32 +74,43 @@ public:
         return this->field_;
     }
 
-    [[nodiscard]] constexpr auto& GetSize() const {
+    [[nodiscard]] constexpr auto GetSize() const {
         return this->boardSize_;
     }
 
-    [[nodiscard]] constexpr auto& GetRows() const {
+    [[nodiscard]] constexpr auto GetRows() const {
         return RowsCount;
     }
 
-    [[nodiscard]] constexpr auto& GetColumns() const {
+    [[nodiscard]] constexpr auto GetColumns() const {
         return ColumnsCount;
     }
 
     //--------------------------------------------- METHOD SECTION ---------------------------------------------------//
 
-    [[nodiscard]] auto GetCell(const size_t row, const size_t column) const {
-        if (row >= RowsCount) {
+    [[nodiscard]] auto GetCell(const Row row, const Column column) const {
+        if (static_cast<short>(row) >= RowsCount) {
             throw std::out_of_range("Row index out of boundaries.");
         }
-        else if (column >= ColumnsCount) {
+        else if (static_cast<short>(column) >= ColumnsCount) {
             throw std::out_of_range("Column index out of boundaries.");
         }
 
-        return this->field_[row * RowsCount + column];
+        return this->field_[static_cast<size_t>(row) * ColumnsCount + static_cast<size_t>(column)];
+    }
+
+    // TODO Player class as second param
+    bool MakeMove(Column column, MoveCharacters move) {
+        auto index = static_cast<short>(column);
+        while (index < this->boardSize_ && this->field_[index] == (char)MoveCharacters::NONE) {
+            index += static_cast<short>(ColumnsCount);
+        }
+
+        this->field_[index - ColumnsCount] = (char)move;
+        return true;
     }
 
 private:
     static constexpr auto boardSize_ = RowsCount * ColumnsCount;
-    std::array<MoveCharacters, Board::boardSize_> field_;
+    std::array<char, Board::boardSize_> field_;
 };
